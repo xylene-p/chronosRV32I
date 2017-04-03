980a335e85e917cfdd73c3ba984c55485bcd3bca
Katherine Perez <khperez@uh.edu>
2017-04-03 05:41:24 -0500

    Init commit to alpha branch

---
+++ src/alu.v
@@ -0,0 +1,15 @@
+// Chronos ALU
+
+`include "defines.vh"
+
+module alu (
+    // outputs
+    output reg [31:0] alu_out;
+    // inputs
+    input      [31:0] op1, op2;
+    input      [3:0]  alu_func;
+    );
+
+
+
+endmodule
