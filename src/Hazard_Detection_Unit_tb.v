cc847ab30acca4942a746d8c12528eb5a7af5790
Sikender Shahid <sikendershahid91@gmail.com>
2017-04-17 13:50:07 -0500

    testbench fixed

--- src/Hazard_Detection_Unit_tb.v
+++ src/Hazard_Detection_Unit_tb.v
@@ -33,23 +33,23 @@ initial begin
 	$display("Testing if returns PASSED for a hazard");
 	// 1st inst register access rd- x1F
 	// 2nd inst register access rs- x1F , x1B
-	IDEX_WRITE_RD = 5'b0; 
-	IFID_WRITE_RS = 5'b0; 
+	IDEX_WRITE_RD = 5'b0;
+	IFID_WRITE_RS = 5'b0;
 	IFID_WRITE_RD = 5'b0;
-	IDEX_MEMWRITE = 1'b1; 
-	$display("%b, %b, %b, %b", IDEX_WRITE_RD, IFID_WRITE_RS, IFID_WRITE_RD, IDEX_MEMWRITE); 
-	if(PC_READ == 0 && IFID_READ == 0&& MUX_SELECT_READ == 1)begin
-		$display("PASSED"); 
+	IDEX_MEMWRITE = 1'b1;
+	#100;
+	$display("%b, %b, %b, %b", IDEX_WRITE_RD, IFID_WRITE_RS, IFID_WRITE_RD, IDEX_MEMWRITE);
+	if(PC_READ == 0 && IFID_READ == 0 && MUX_SELECT_READ == 1) begin
+		$display("PASSED");
 	end
 	else begin
 		$display("FAILED");
 	end
-	$display("PC_READ - %d \t IFID_READ - %d \t MUX_SELECT_READ - %d", 
-			PC_READ, 
-			IFID_READ, 
-			MUX_SELECT_READ); 
-	#100 $finish; 
-
+	$display("PC_READ - %d \t IFID_READ - %d \t MUX_SELECT_READ - %d",
+			_firstTestInstance.PC_write,
+			_firstTestInstance.IFID_write,
+			_firstTestInstance.Mux_select);
+    $finish;
 end
 
 endmodule
