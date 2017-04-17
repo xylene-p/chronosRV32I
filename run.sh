0b1312a838e260659212d85ba4857a3ff9052ac4
Katherine Perez <khperez@uh.edu>
2017-04-17 17:29:59 -0500

    Update shell file to work on any system

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
-iverilog -I/home/exmachina/chronosRV32I/inc -o output/coreTest.out src/*.v test/*.v
+root=$(pwd)
+iverilog -I$root/inc -o output/coreTest.out src/*.v test/*.v
 vvp output/coreTest.out
 gtkwave core.vcd &
