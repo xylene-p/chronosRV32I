721601047f93f618b2d4c9b616ac72aa663ca744
Sikender Shahid <sikendershahid91@gmail.com>
2017-04-21 07:26:42 -0500

    Initializing memwb register I/O

---
+++ src/MEMWBRegister.v
@@ -0,0 +1,20 @@
+module MEMWBRegister(
+//outputs
+	WBData_out; 
+	//controls to WB
+	wb_sel_out; 
+	register_write_enable_out; 
+
+//inputs
+	WBData_in; 
+	clk; 
+	rst; 
+	en; 
+	//controls to WB
+	wb_sel_in; 
+	register_write_enable_in; 
+	); 
+
+
+
+endmodule 
