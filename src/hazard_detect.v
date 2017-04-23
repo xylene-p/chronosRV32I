19f55475e9d0f11510e254900355e38e32e0d4e3
Sikender Shahid <sikendershahid91@gmail.com>
2017-04-22 21:38:21 -0500

    variable changes in wb, Hazard_detect

--- src/hazard_detect.v
+++ src/hazard_detect.v
@@ -4,73 +4,64 @@
 `include "defines.vh"
 
 module hazard_detect(
-	//output
-	PC_write, 	// needs to be changed 
-	IFID_write,
-	Mux_select,
-	//inputs
-	inst_type,
-	IFID_Reg_Rs1,
-	IFID_Reg_Rs2,
-	IFID_Reg_Rd,
-	IDEX_MemRead,
-	IDEX_Reg_Rd
-
-	//HDU controlsignal 
-	IF_kill, 
-	DEC_kill, 
+	output reg pc_sel,
+	output reg IFID_write,
+	output reg mux_select,
+	output reg kill_IF, 
+	output reg kill_DEC,
+	input [6:0] opcode,
+	input [4:0] IFID_Reg_Rs1,
+	input [4:0] IFID_Reg_Rs2,
+	input [4:0] IFID_Reg_Rd,
+	input [4:0] IDEX_Reg_Rd
+	input IDEX_MemRead,
 	);
 
-output reg PC_write, IFID_write, Mux_select, IF_kill, DEC_kill;
-input [4:0] IFID_Reg_Rs1, IFID_Reg_Rs2, IFID_Reg_Rd, IDEX_Reg_Rd;
-input IDEX_MemRead;
-input [6:0] inst_type;
-
 always @(*) begin
-	case (inst_type)
+	case (opcode)
 		`OPCODE_OP: begin// R-type instruction
 			if (IDEX_MemRead && (IDEX_Reg_Rd == IFID_Reg_Rs1 ||
 								 IDEX_Reg_Rd == IFID_Reg_Rs2 ||
 								 IDEX_Reg_Rd == IFID_Reg_Rd))
 			begin
-				PC_write <= 0;
+				pc_sel <= 0;
 				IFID_write <= 0;
-				Mux_select <= 1;
-				IF_kill <= 1; 
-				DEC_kill <= 1; 
+				mux_select <= 1;
+				kill_IF <= 1; 
+				kill_DEC <= 1; 
 			end
 			else begin
-				PC_write <= 1;
+				pc_sel <= 1;
 				IFID_write <= 1;
-				Mux_select <= 0;
-				IF_kill <= 0; 
-				DEC_kill <= 0; 
+				mux_select <= 0;
+				kill_IF <= 0; 
+				kill_DEC <= 0; 
 			end
 		end
 		`OPCODE_OP_IMM: begin // I-type instruction
 			if (IDEX_MemRead && (IDEX_Reg_Rd == IFID_Reg_Rs1 ||
 								 IDEX_Reg_Rd == IFID_Reg_Rd))
 			begin
-				PC_write <= 0;
+				pc_sel <= 0;
 				IFID_write <= 0;
- 				Mux_select <= 1;
- 				IF_kill <= 1; 
-				DEC_kill <= 1; 
+ 				mux_select <= 1;
+ 				kill_IF <= 1; 
+				kill_DEC <= 1; 
  			end
  			else begin
- 				PC_write <= 1;
+ 				pc_sel <= 1;
  				IFID_write <= 1;
- 				Mux_select <= 0;
- 				IF_kill <= 0; 
-				DEC_kill <= 0; 
+ 				mux_select <= 0;
+ 				kill_IF <= 0; 
+				kill_DEC <= 0; 
  			end
 		end
 		default: begin
-			PC_write <= 1;
+			pc_sel <= 1;
 			IFID_write <= 1;
-			Mux_select <= 0;
-			IF_kill <= 0; 
-			DEC_kill <= 0; 
+			mux_select <= 0;
+			kill_IF <= 0; 
+			kill_DEC <= 0; 
 		end
 	endcase
 end
